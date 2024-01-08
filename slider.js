d2886bb8a08ae76d8d07ea215105ee9b5c1a857b
hollydainton <ws231710@weston.ac.uk>
2024-01-08 23:08:44 +0000

    code

---
+++ slider.js
@@ -0,0 +1,21 @@
+// const swiper1 = new Swiper(".slider-1", {
+//   autoplay: {
+//     delay: 3500,
+//     disableOnInteraction: false,
+//   },
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-next",
+//     prevEl: ".swiper-prev",
+//   },
+// });
+
+/*about slider*/
+const swiper2 = new Swiper(".slider-2", {
+  autoHeight: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
